79cb831f02c57d8c001512a821e24ef581529ad7
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-02 15:11:39 +0100

    Setting state

--- App.jsx
+++ App.jsx
@@ -3,35 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: []
+        }
+
+        this.setStateHandler = this.setStateHandler.bind(this);
+    };
+    setStateHandler() {
+        var item = "setState...";
+        var myArray = this.state.data.slice();
+        myArray.push(item);
+        this.setState({data: myArray})
+    };
     render() {
         return (
             <div>
-                <h1> Hello, {this.props.name} </h1>
-                <h3>Array: {this.props.propArray}</h3>
-                <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-                <h3>Func: {this.props.propFunc(3)}</h3>
-                <h3>Number: {this.props.propNumber}</h3>
-                <h3>String: {this.props.propString}</h3>
+                <button onClick = {this.setStateHandler}>SET STATE</button>
+                <h4>State Array: {this.state.data}</h4>
             </div>
         );
     }
-}
-App.propTypes = {
-    name: PropTypes.string,
-    propArray: PropTypes.array.isRequired,
-    propBool: PropTypes.bool.isRequired,
-    propFunc: PropTypes.func,
-    propNumber: PropTypes.number,
-    propString: PropTypes.string,
-};
-App.defaultProps = {
-    name: 'Tutorialspoint.com',
-    propArray: [1, 2, 3, 4, 5],
-    propBool: true,
-    propFunc: function(e) {
-        return e
-    },
-    propNumber: 1,
-    propString: "String value..."
 }
 export default App;
