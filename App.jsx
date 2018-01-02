01983f974c222579c681b959face2d3cbc63d1f3
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-02 14:06:59 +0100

    Passing states by using props

--- App.jsx
+++ App.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            header: "Header from props...",
+            content: "Content from props..."
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>{this.props.headerProp}</h1>
-                <h2>{this.props.contentProp}</h2>
+                <Header headerProp={this.state.header}/>
+                <Content contentProp={this.state.content}/>
             </div>
         );
     }
 }
-App.defaultProps = {
-    headerProp: "Header from props...",
-    contentProp:"Content from props default..."
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.headerProp}</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.contentProp}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
