70f14e81f57940279a12640226f7f3481a5bdfdd
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-02 16:04:14 +0100

    More complex example of updating state - passing state as property to child and update state from child

--- App.jsx
+++ App.jsx
@@ -15,9 +15,19 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <input type = "text" value = {this.state.data}
-                       onChange = {this.updateState} />
-                <h4>{this.state.data}</h4>
+                <Content myDataProp = {this.state.data}
+                         updateStateProp = {this.updateState}></Content>
+            </div>
+        );
+    }
+}
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type = "text" value = {this.props.myDataProp}
+                       onChange = {this.props.updateStateProp} />
+                <h3>{this.props.myDataProp}</h3>
             </div>
         );
     }
