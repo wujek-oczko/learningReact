48963ca8e5f2f523ab3ca6778b1b72e51e2b3eb1
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-02 16:24:32 +0100

    Using keys

--- App.jsx
+++ App.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
-            data: ''
+            data:[
+                {
+                    component: 'First...',
+                    id: 1
+                },
+                {
+                    component: 'Second...',
+                    id: 2
+                },
+                {
+                    component: 'Third...',
+                    id: 3
+                }
+            ]
         }
-        this.updateState = this.updateState.bind(this);
-        this.clearInput = this.clearInput.bind(this);
-    };
-    updateState(e) {
-        this.setState({data: e.target.value});
-    }
-    clearInput() {
-        this.setState({data: ''});
-        ReactDOM.findDOMNode(this.refs.myInput).focus();
     }
     render() {
         return (
             <div>
-                <input value = {this.state.data} onChange = {this.updateState}
-                       ref = "myInput"></input>
-                <button onClick = {this.clearInput}>CLEAR</button>
-                <h4>{this.state.data}</h4>
+                <div>
+                    {this.state.data.map((dynamicComponent, i) => <Content
+                        key = {i} componentData = {dynamicComponent}/>)}
+                </div>
+            </div>
+        );
+    }
+}
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>{this.props.componentData.component}</div>
+                <div>{this.props.componentData.id}</div>
             </div>
         );
     }
