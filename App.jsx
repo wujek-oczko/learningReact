780f1513285ecc6e91ceee33fbe1c797fa98aef6
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-02 13:54:43 +0100

    Props

--- App.jsx
+++ App.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            header: "Header from state...",
-            content: "Content from state..."
-        }
-    }
     render() {
         return (
             <div>
-                <h1>{this.state.header}</h1>
-                <h2>{this.state.content}</h2>
+                <h1>{this.props.headerProp}</h1>
+                <h2>{this.props.contentProp}</h2>
             </div>
         );
     }
