a0b644cb944fbe006dbd6a5834ab953e0c6f5723
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-03 09:48:42 +0100

    Router finally working

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-var config = {
-   entry: './main.js',
-
-   output: {
-      path:'/',
-      filename: 'index.js',
-   },
-   devServer: {
-      inline: true,
-      port: 2080
-   },
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
     module: {
         loaders: [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
                 loader: 'babel-loader',
-
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                test: /\.js$/,
+                exclude: /node_modules/
             }
         ]
+    },
+    devServer: {
+        port: 2080
     }
-}
-module.exports = config;
+};
