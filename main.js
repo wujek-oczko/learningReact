39c5e1525b0476c73e51c6a0ff78befd4bbf8270
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-01-03 13:42:35 +0100

    Higher order controller

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+// import App from './App.jsx';
+import MyHOC from './MyHOC.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<MyHOC />, document.getElementById('app'));
